563beb04b2edce7ac2435bd39b0fac66cea49e7f
CristinaOsorio <macry_08@hotmail.com>
2023-05-07 01:37:54 -0600

    create post method in product

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,18 +1,28 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import { Controller, Get, Post, Query, Param, Body } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  getProducts(
+  getAll(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: number,
-  ): string {
-    return `products: limit ${limit} & offset ${offset} & brand ${brand}`;
+  ) {
+    return {
+      message: `products: limit ${limit} & offset ${offset} & brand ${brand}`,
+    };
   }
 
   @Get(':productId')
-  getProduct(@Param('productId') productId: string): string {
+  getOne(@Param('productId') productId: string): string {
     return `product: ${productId}`;
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'create product',
+      payload,
+    };
   }
 }
