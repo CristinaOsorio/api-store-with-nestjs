8f92f6b6ff8855a2ecc2816e8069dea7ce9bc835
CristinaOsorio <macry_08@hotmail.com>
2023-10-25 14:23:57 -0600

    mongo installation and configuration start

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,21 @@
 import { Global, Module } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
 
 @Global()
 @Module({
-  providers: [],
-  exports: [],
+  providers: [
+    {
+      provide: 'MONGO',
+      useFactory: async () => {
+        const uri =
+          'mongodb://root:root@localhost:27017/?authMechanism=DEFAULT';
+        const client = new MongoClient(uri);
+        await client.connect();
+        const database = client.db('store-nestjs');
+        return database;
+      },
+    },
+  ],
+  exports: ['MONGO'],
 })
 export class DatabaseModule {}
