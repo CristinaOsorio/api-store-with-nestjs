0e4c173fd324af9bfe509c09e9008efc8d683fb5
CristinaOsorio <macry_08@hotmail.com>
2023-10-25 16:10:11 -0600

    using mongoose to connect to database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,25 +1,26 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MongoClient } from 'mongodb';
 import config from 'config';
-import { ConfigType } from '@nestjs/config';
 
 @Global()
 @Module({
-  providers: [
-    {
-      provide: 'MONGO',
-      useFactory: async (configService: ConfigType<typeof config>) => {
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
         const { connection, user, dbName, password, host, port } =
           configService.mongo;
-        const uri = `${connection}://${user}:${password}@${host}:${port}/?authMechanism=DEFAULT`;
-        const client = new MongoClient(uri);
-        await client.connect();
-        const database = client.db(dbName);
-        return database;
+        return {
+          uri: `${connection}://${host}:${port}`,
+          user,
+          pass: password,
+          dbName,
+        };
       },
       inject: [config.KEY],
-    },
+    }),
   ],
-  exports: ['MONGO'],
+  exports: [MongooseModule],
 })
 export class DatabaseModule {}
