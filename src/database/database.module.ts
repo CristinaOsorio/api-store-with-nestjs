54eb5ab64e0a353cd68c1f3c9776c10b0f17465e
CristinaOsorio <macry_08@hotmail.com>
2023-10-25 15:55:03 -0600

    Using environment variables in Mongo

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,19 +1,23 @@
 import { Global, Module } from '@nestjs/common';
 import { MongoClient } from 'mongodb';
+import config from 'config';
+import { ConfigType } from '@nestjs/config';
 
 @Global()
 @Module({
   providers: [
     {
       provide: 'MONGO',
-      useFactory: async () => {
-        const uri =
-          'mongodb://root:root@localhost:27017/?authMechanism=DEFAULT';
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const { connection, user, dbName, password, host, port } =
+          configService.mongo;
+        const uri = `${connection}://${user}:${password}@${host}:${port}/?authMechanism=DEFAULT`;
         const client = new MongoClient(uri);
         await client.connect();
-        const database = client.db('store-nestjs');
+        const database = client.db(dbName);
         return database;
       },
+      inject: [config.KEY],
     },
   ],
   exports: ['MONGO'],
