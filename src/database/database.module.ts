45c53bf6322e706bff9b750e51f42cf6ba89b762
CristinaOsorio <macry_08@hotmail.com>
2023-11-03 21:39:01 -0600

    Integration and configuration of node-postgres with NestJS

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,29 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
+import config from 'config';
 
 @Global()
 @Module({
-  providers: [],
-  exports: [],
+  providers: [
+    {
+      provide: 'PG',
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { db, user, password, host, port } = configService.postgres;
+        const client = new Client({
+          user,
+          host,
+          password,
+          database: db,
+          port,
+        });
+
+        client.connect();
+        return client;
+      },
+      inject: [config.KEY],
+    },
+  ],
+  exports: ['PG'],
 })
 export class DatabaseModule {}
