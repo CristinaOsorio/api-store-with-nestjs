d1bb92b36ad677d98429c483cfc593d3e22af30c
CristinaOsorio <macry_08@hotmail.com>
2023-11-14 03:01:59 -0600

    ParseIntPipe pipe return fix

--- src/common/pipes/parse-int/parse-int.pipe.ts
+++ src/common/pipes/parse-int/parse-int.pipe.ts
@@ -9,10 +9,9 @@ import {
 export class ParseIntPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata) {
     const val = parseInt(value, 10);
-
     if (isNaN(val)) {
       throw new BadRequestException(`${value} is not an number`);
     }
-    return value;
+    return val;
   }
 }
